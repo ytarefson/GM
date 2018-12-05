36901d0104fa43ce0450c0048738cdddd23e21fd
FoX <cs_fox@mail.ru>
2018-12-05 14:28:50 +0300

    111

--- components/products/ProductUnitPlate.js
+++ components/products/ProductUnitPlate.js
@@ -30,37 +30,6 @@ export class ProductsUnitPlate extends Component {
       }
     });
     return (
-<<<<<<< HEAD
-      <div>
-        <Head
-          title={`Продукт - ${product.label}`}
-          description="Лучший продукт"
-        />
-        <div className="container-fluid product-unit">
-          <div className="mycontainer section-plate">
-            <div className="breadcrumbs">
-              <Link href="/">
-                <a className="crambs">Главная > </a>
-              </Link>
-              <Link href="/products">
-                <a className="crambs">Продукты > </a>
-              </Link>
-              <Link href={`/products?category=${category}`}>
-                <a className="crambs">{category} > </a>
-              </Link>
-              <span className="crambs">{product.label}</span>
-            </div>
-          </div>
-          <div className="mycontainer section-plate">
-            <div className="row m-0 pt-3 pb-3">
-              <div className="col-12 col-md-8 order-md-1 order-2">
-                <h2 className="product-header">Описание {product.label}</h2>
-                <span className="product-energy">
-                  Мощность: {product.electricPower} кВт
-                </span>
-                <p className="text-min">{product.description}</p>
-              </div>
-=======
       <div className="container-fluid product-unit">
         <div className="mycontainer section-plate">
           <div className="breadcrumbs">
@@ -84,7 +53,6 @@ export class ProductsUnitPlate extends Component {
                 Мощность: {product.electricPower}
               </span>
               <p className="text-min">{product.description}</p>
->>>>>>> parent of c50d78a... PRODUCT CARD UNIT styled
             </div>
             <div className="mycontainer section-plate">
               <div className="row m-0 pt-3 pb-3">
@@ -108,7 +76,6 @@ export class ProductsUnitPlate extends Component {
               </div>
             </div>
           </div>
-<<<<<<< HEAD
           <div className="mycontainer section-plate">
             <h3 className="product-header pl-3">
               Характеристики {product.label}:
@@ -204,97 +171,6 @@ export class ProductsUnitPlate extends Component {
           </div>
           <SimilarProducts similar={similar} category={category} />
         </div>{" "}
-=======
-        </div>
-        <SimilarProducts similar={similar} category={category} />
-        <div className="mycontainer section-plate">
-          <h3 className="product-header pl-3">
-            Характеристики {product.label}:
-          </h3>
-          <div className="row m-0">
-            <div className="col-12 col-md-6">
-              <span className="text-common">Марка двигателя: </span>
-              <span className="text-bold">{product.motorMark}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Газовоздушный смеситель: </span>
-              <span className="text-bold">{product.gasMixer}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Блок управления: </span>
-              <span className="text-bold">{product.controlBlock}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Система зажигания: </span>
-              <span className="text-bold">{product.ignitionSystem}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Дроссельная заслонка: </span>
-              <span className="text-bold">{product.throttleValve}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">
-                Коэффициент избытка воздуха, λ:{" "}
-              </span>
-              <span className="text-bold">{product.excessAirRatio}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Напряжение, В</span>
-              <span className="text-bold">{product.voltage}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Электрическая мощность, кВт: </span>
-              <span className="text-bold">{product.electricPower}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Тепловая мощность, кВт: </span>
-              <span className="text-bold">{product.thermalPower}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">КПД электрический, %: </span>
-              <span className="text-bold">{product.electricEfficiency}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">КПД тепловой, %: </span>
-              <span className="text-bold">{product.thermalEfficiency}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">КПД общий, %: </span>
-              <span className="text-bold">{product.overallEfficiency}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Тип топлива: </span>
-              <span className="text-bold">{product.fuelType}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Метановое число: </span>
-              <span className="text-bold">{product.methaneNumber}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Давление газа, кгс/см2: </span>
-              <span className="text-bold">{product.gasPressure}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">
-                Расход газа в номинальном режиме, нм3/час:{" "}
-              </span>
-              <span className="text-bold">{product.nominalGazConsumption}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Уровень шума, Дб: </span>
-              <span className="text-bold">{product.noiseLevel}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Сервисный интервал: </span>
-              <span className="text-bold">{product.serviceInterval}</span>
-            </div>
-            <div className="col-12 col-md-6">
-              <span className="text-common">Общий ресурс электростанции: </span>
-              <span className="text-bold">{product.totalPowerPlant}</span>
-            </div>
-          </div>
-        </div>
->>>>>>> parent of c50d78a... PRODUCT CARD UNIT styled
       </div>
     );
   }
