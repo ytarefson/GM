77cd842b62ad3443300ad6db37771d41b9ea991d
yta <ytarefson@gmail.com>
2018-12-05 12:27:20 +0300

    portfolio links fixed on main page

--- components/portfolioLinks.js
+++ components/portfolioLinks.js
@@ -1,32 +1,36 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 //import Router from 'next/router';
-import "../scss/portfolioLinks.scss";
+import '../scss/portfolioLinks.scss';
 
 const portfolio = [
   {
-    label: "Аккус Актобэ",
-    company: "Энергоцентр",
-    href: "#",
-    as: "/akkus-aktobe"
+    label: 'Аккус Актобэ',
+    company: 'Энергоцентр',
+    href: '#',
+    id: '3',
+    as: '/akkus-aktobe'
   },
   {
-    label: "Ледовая Арена",
-    company: "Энергоцентр",
-    href: "#",
-    as: "/ledovaya-arena"
+    label: 'Ледовая Арена',
+    company: 'Энергоцентр',
+    href: '#',
+    id: '',
+    as: '/ledovaya-arena'
   },
   {
-    label: "Невская ратуша",
-    company: "Энергоцентр",
-    href: "#",
-    as: "/nevskaya-ratusha"
+    label: 'Невская ратуша',
+    company: 'Энергоцентр',
+    href: '#',
+    id: '9',
+    as: '/nevskaya-ratusha'
   },
   {
-    label: "СтройГазМонтаж",
-    company: "Энергоцентр",
-    href: "#",
-    as: "/stroi-gaz-montazh"
+    label: 'СтройГазМонтаж',
+    company: 'Энергоцентр',
+    href: '#',
+    id: '5',
+    as: '/stroi-gaz-montazh'
   }
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`;
@@ -39,12 +43,14 @@ const PortfolioLinks = () => (
       <span className="portfolio-header d-block">Реализованные проекты:</span>
     </div>
     <ul className="portfolio-ul">
-      {portfolio.map(({ key, href, label, as, company }) => (
-        <Link href={href} as={as} key={key}>
-          <li className="portfolio-li">
-            <span className="portfolio-company">{company}</span>
-            <a className="portfolio-link">{label}</a>
-          </li>
+      {portfolio.map(({ key, id, label, as, company }) => (
+        <Link href={`/proekti/${id}`} key={key}>
+          <a className="portfolio-a">
+            <li className="portfolio-li">
+              <span className="portfolio-company">{company}</span>
+              <span className="portfolio-link">{label}</span>
+            </li>
+          </a>
         </Link>
       ))}
     </ul>
