c211d847cfc4f19f9234ee38e8bce6b204f84e0e
FoX <cs_fox@mail.ru>
2018-12-27 18:30:00 +0300

    pre ICON save

--- components/portfolioLinks.js
+++ components/portfolioLinks.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 //import Router from 'next/router';
-import '../scss/portfolioLinks.scss';
+import "../scss/portfolioLinks.scss";
 
 const portfolio = [
   {
-    label: 'Аккус Актобэ',
-    company: 'Энергоцентр',
-    href: '#',
-    id: '3',
-    as: '/akkus-aktobe'
+    label: "Аккус Актобэ",
+    company: "Энергоцентр",
+    href: "#",
+    id: "3",
+    as: "/akkus-aktobe"
   },
   {
-    label: 'Ледовая Арена',
-    company: 'Энергоцентр',
-    href: '#',
-    id: '',
-    as: '/ledovaya-arena'
+    label: "Ледовая Арена",
+    company: "Энергоцентр",
+    href: "#",
+    id: "",
+    as: "/ledovaya-arena"
   },
   {
-    label: 'Невская ратуша',
-    company: 'Энергоцентр',
-    href: '#',
-    id: '9',
-    as: '/nevskaya-ratusha'
+    label: "Невская ратуша",
+    company: "Энергоцентр",
+    href: "#",
+    id: "9",
+    as: "/nevskaya-ratusha"
   },
   {
-    label: 'СтройГазМонтаж',
-    company: 'Энергоцентр',
-    href: '#',
-    id: '5',
-    as: '/stroi-gaz-montazh'
+    label: "СтройГазМонтаж",
+    company: "Энергоцентр",
+    href: "#",
+    id: "5",
+    as: "/stroi-gaz-montazh"
   }
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`;
