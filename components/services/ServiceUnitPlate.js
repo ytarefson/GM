1105d91fe601fc3a9dd135ee9171bc824c8a68a0
yta <ytarefson@gmail.com>
2019-01-28 19:06:03 +0300

    h1 fixed

--- components/services/ServiceUnitPlate.js
+++ components/services/ServiceUnitPlate.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import Head from "../../components/head";
-import Link from "next/link";
-import serviceList from "../tables/serviceList";
-import "../../scss/services/serviceUnitPlate.scss";
+import React, { Component } from 'react';
+import Head from '../../components/head';
+import Link from 'next/link';
+import serviceList from '../tables/serviceList';
+import '../../scss/services/serviceUnitPlate.scss';
 
 export class ServiceUnitPlate extends Component {
   render() {
@@ -31,7 +31,7 @@ export class ServiceUnitPlate extends Component {
           <div className="mycontainer section-plate">
             <div className="row m-0">
               <div className="col-12 col-md-8 card-content">
-                <h2 className="header-red">{service.label}</h2>
+                <h1 className="header-red">{service.label}</h1>
                 <p className="common-text pb-3">{service.text}</p>
               </div>
               <div className="col-12 col-md-4 p-0">
