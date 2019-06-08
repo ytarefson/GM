7c801faeea585bfc66f3faf289cead18ec2973de
FoX <cs_fox@mail.ru>
2019-06-08 19:19:59 +0300

    all restyled 100%

--- components/services/ServiceUnitPlate.js
+++ components/services/ServiceUnitPlate.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import Head from '../../components/head';
-import Link from 'next/link';
-import serviceList from '../tables/serviceList';
-import '../../scss/services/serviceUnitPlate.scss';
+import React, { Component } from "react";
+import Head from "../../components/head";
+import Link from "next/link";
+import serviceList from "../tables/serviceList";
+import "../../scss/services/serviceUnitPlate.scss";
 
 export class ServiceUnitPlate extends Component {
   render() {
@@ -37,7 +37,7 @@ export class ServiceUnitPlate extends Component {
         <section className="service-unit container-fluid color-section">
           <div className="mycontainer section-plate">
             <div className="row m-0">
-              <div className="col-12 col-md-4 col-lg-5 p-0">
+              <div className="col-12 col-md-6 col-lg-5 p-0">
                 <div className="card-img-container">
                   <img
                     src={service.imgDetail}
@@ -47,9 +47,29 @@ export class ServiceUnitPlate extends Component {
                   />
                 </div>
               </div>
-              <div className="col-12 col-md-7 col-lg-6 col-xl-6 offset-md-1 card-content p-0 p-md-2">
-                <h1 className="header-red">{service.label}</h1>
-                <p className="text pb-3">{service.text}</p>
+              <div className="col-12 col-md-6  col-lg-7 col-xl-7  card-content p-0">
+                <div className="content-wrapper">
+                  <div className="top-content">
+                    <h1 className="header-red">{service.label}</h1>
+                    <p className="text pb-3">{service.text}</p>
+                  </div>
+                  <div className="bottom-content">
+                    <div className="button-group">
+                      <div className="button-container-new">
+                        <a
+                          href="https://gmenergo.bitrix24.ru/pub/form/4_uznayte_vse_podrobnosti_i_usloviya_sotrudnichestva/c1mwzy/"
+                          className="button-new"
+                        >
+                          Узнайте подробности
+                        </a>
+                      </div>
+                      <p className="arenda-text-min">
+                        Оставьте контактную информацию и наши специалисты
+                        подробно расскажут об условиях сотрудничества.
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
