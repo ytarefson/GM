b2ef81279b20fd2dbb54f8b4641f706b8cd88795
yta <ytarefson@gmail.com>
2019-06-25 02:20:00 +0300

    fix

--- pages/confirm-email.js
+++ pages/confirm-email.js
@@ -1,38 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Head from '../components/head';
 import Layout from '../components/Layout';
 import Link from 'next/link';
 import '../scss/confirmEmail.scss';
 import ym from 'react-yandex-metrika';
 
-class  ConfirmEmailSend extends Component {
+class ConfirmEmailSend extends Component {
   componentDidMount() {
     ym('hit', '/confirm-email');
   }
   render() {
     return (
-  <div>
-    <Head
-      title="Сообщение успешно отправлено - Газовые машины"
-      description="Если вы на этой странице, значит Ваше сообщение отправлено"
-    />
+      <div>
+        <Head
+          title="Сообщение успешно отправлено - Газовые машины"
+          description="Если вы на этой странице, значит Ваше сообщение отправлено"
+        />
 
-    <Layout>
-      <div className="container-fluid">
-        <div className="mycontainer">
-          <div className="success-message">
-            <h1 className="success header-razdel">
-              Ваше сообщение успешно отправлено
-            </h1>
-            <Link href="/">
-              <a className="success-link text-common">Вернуться на главную</a>
-            </Link>
+        <Layout>
+          <div className="container-fluid">
+            <div className="mycontainer">
+              <div className="success-message">
+                <h1 className="success header-razdel">
+                  Ваше сообщение успешно отправлено
+                </h1>
+                <Link href="/">
+                  <a className="success-link text-common">
+                    Вернуться на главную
+                  </a>
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
+        </Layout>
       </div>
-    </Layout>
-  </div>
-);
-    ]}
+    );
+  }
+}
 
 export default ConfirmEmailSend;
